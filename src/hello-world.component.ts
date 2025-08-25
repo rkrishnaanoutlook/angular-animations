af872db8351d67e1cae15d7f7ba909709c1a9355
rkrishnaan@outlook.com <98129317+rkrishnaanoutlook@users.noreply.github.com>
2025-08-25 14:11:14 -0400

    Trigger sparkle burst at click position

--- src/hello-world.component.ts
+++ src/hello-world.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HelloWorldComponent implements OnInit {
   showTip = false;
   sparkle = false;
+  sparkleX = 0;
+  sparkleY = 0;
   particles = Array(8);
 
   ngOnInit() {
@@ -16,7 +18,11 @@ export class HelloWorldComponent implements OnInit {
     setTimeout(() => (this.showTip = false), 6000);
   }
 
-  triggerSparkle() {
+  triggerSparkle(event: MouseEvent) {
+    const target = event.currentTarget as HTMLElement;
+    const rect = target.getBoundingClientRect();
+    this.sparkleX = event.clientX - rect.left;
+    this.sparkleY = event.clientY - rect.top;
     this.sparkle = false;
     requestAnimationFrame(() => (this.sparkle = true));
   }
